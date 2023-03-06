39a486f00635f609970004a50cc805b8280323bd
nonorze-VOXX <appieosugood@gmail.com>
2023-03-06 02:30:21 -0800

    normalize function and vector2 struct

--- Source/Game/BtdClass/BtdUtil.h
+++ Source/Game/BtdClass/BtdUtil.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "GameObject.h"
 
+
 namespace Btd
 {
-    Vector2 Normailize(float x, float y)
+    struct Vector2
     {
-        float powSum = sqrt(x * x + y * y);
-        return {x / powSum, y / powSum};
-    }
+        float X;
+        float Y;
+    };
+
+    Vector2 Normailize(float x, float y);
 }
