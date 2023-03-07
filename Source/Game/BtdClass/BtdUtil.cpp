d7ec26b7f433c2023cc7708c7673235eb4f08149
nonorze-VOXX <appieosugood@gmail.com>
2023-03-07 12:54:58 +0800

    add compare vector2 ,normalize(vector2)

--- Source/Game/BtdClass/BtdUtil.cpp
+++ Source/Game/BtdClass/BtdUtil.cpp
@@ -8,4 +8,11 @@ namespace Btd
         float powSum = sqrt(x * x + y * y);
         return {x / powSum, y / powSum};
     }
+    Vector2 Normailize(Vector2 v){
+        float powSum = sqrt(v.X * v.X + v.Y * v.Y);
+        return {v.X / powSum, v.Y / powSum};
+    }
+    bool CompareVector2(Vector2 a,Vector2 b){
+        return a.X == b.X && a.Y==b.Y;
+    }
 }
