5c9c33e1e7c994753c16a3429416b80173869bc8
huhuhu0420 <ghostcheng.00@gmail.com>
2023-03-27 12:20:26 +0800

    fix nail machine shoot when not active

--- Source/Game/BtdClass/NailMachine.cpp
+++ Source/Game/BtdClass/NailMachine.cpp
@@ -13,25 +13,28 @@ namespace Btd
 
     void NailMachine::Update()
     {
-        UpdateThrowable();
+        if (_isActive)
+        {
+            UpdateThrowable();
 
-        if (!BloonFactory::BloonVector.empty() && GetShootTimeCounter() > GetShootDeltaTime())
-        {
-            Bloon targetBloon = focus();
-            Vector2 direction = {1, 0};
-            for (int i = 0; i < 8; i++)
+            if (!BloonFactory::BloonVector.empty() && GetShootTimeCounter() > GetShootDeltaTime())
             {
-                Vector2 target = {
-                    GetLeft() + direction.X,
-                    GetTop()+ direction.Y
-                };
-                Shoot(target);
-                direction = Spin45(direction);
+                Bloon targetBloon = focus();
+                Vector2 direction = {1, 0};
+                for (int i = 0; i < 8; i++)
+                {
+                    Vector2 target = {
+                        GetLeft() + direction.X,
+                        GetTop()+ direction.Y
+                    };
+                    Shoot(target);
+                    direction = Spin45(direction);
+                }
             }
-        }
-        else
-        {
-            SetShootTimeCounter(GetShootTimeCounter() + deltaTime / 100.F);
+            else
+            {
+                SetShootTimeCounter(GetShootTimeCounter() + deltaTime / 100.F);
+            }
         }
     }
 
