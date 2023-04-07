4b228d5605a458a1895eba69d302a47a72a7ef25
nonorze-VOXX <appieosugood@gmail.com>
2023-04-06 19:55:18 -0700

    fix route broke

--- Source/Game/BtdClass/GameManager.cpp
+++ Source/Game/BtdClass/GameManager.cpp
@@ -13,26 +13,25 @@ namespace Btd
             static_cast<float>(map->GetBackground().GetHeight())
         };
         map->InitFactoryButton();
-        map->SetStartPosition({static_cast<float>(0), mapSize.Y * 0.4F});
+        map->SetStartPosition({static_cast<float>(0), mapSize.Y * 0.5F});
         live = map->InitLives;
         money = map->InitMoney;
         db.LoadRounds();
         map->SetRoute({
-            {mapSize.X * 0.11F, mapSize.Y * 0.4F},
-            {mapSize.X * 0.11F, mapSize.Y * 0.12F},
-            {mapSize.X * 0.3F, mapSize.Y * 0.12F},
-            {mapSize.X * 0.3F, mapSize.Y * 0.67F},
-            {mapSize.X * 0.05F, mapSize.Y * 0.67F},
-            {mapSize.X * 0.05F, mapSize.Y * 0.85F},
-            {mapSize.X * 0.65F, mapSize.Y * 0.85F},
-            {mapSize.X * 0.65F, mapSize.Y * 0.53F},
-            {mapSize.X * 0.45F, mapSize.Y * 0.53F},
-            {mapSize.X * 0.45F, mapSize.Y * 0.3F},
-            {mapSize.X * 0.65F, mapSize.Y * 0.3F},
-            {mapSize.X * 0.65F, mapSize.Y * 0.06F},
-            {mapSize.X * 0.38F, mapSize.Y * 0.06F},
-            {mapSize.X * 0.38F, mapSize.Y * 0.F},
-            {mapSize.X * 0.38F, mapSize.Y * -0.08F},
+            {mapSize.X * 0.14F, mapSize.Y * 0.5F},
+            {mapSize.X * 0.14F, mapSize.Y * 0.22F},
+            {mapSize.X * 0.32F, mapSize.Y * 0.22F},
+            {mapSize.X * 0.32F, mapSize.Y * 0.72F},
+            {mapSize.X * 0.07F, mapSize.Y * 0.72F},
+            {mapSize.X * 0.07F, mapSize.Y * 0.90F},
+            {mapSize.X * 0.67F, mapSize.Y * 0.90F},
+            {mapSize.X * 0.67F, mapSize.Y * 0.63F},
+            {mapSize.X * 0.47F, mapSize.Y * 0.63F},
+            {mapSize.X * 0.47F, mapSize.Y * 0.4F},
+            {mapSize.X * 0.67F, mapSize.Y * 0.4F},
+            {mapSize.X * 0.67F, mapSize.Y * 0.13F},
+            {mapSize.X * 0.40F, mapSize.Y * 0.13F},
+            {mapSize.X * 0.40F, mapSize.Y * -0.18F},
         });
         map->SetRounds(db.GetRounds());
         BloonFactory::SetNextRound(map->GetRounds()[round]);
@@ -46,7 +45,9 @@ namespace Btd
         startButton.SetTopLeft(742, 620);
     }
 
-void GameManager::OnKeyUp(UINT, UINT, UINT) {}
+    void GameManager::OnKeyUp(UINT, UINT, UINT)
+    {
+    }
 
     void GameManager::OnLButtonDown(UINT nFlags, CPoint point)
     {
@@ -74,31 +75,39 @@ void GameManager::OnKeyUp(UINT, UINT, UINT) {}
         }
     }
 
-void GameManager::OnLButtonUp(UINT nFlags, CPoint point) {}
-
-void GameManager::OnMouseMove(UINT nFlags, CPoint point) {
-  if (!TowerFactory::TowerVector.empty() &&
-      TowerFactory::TowerVector.back()->IsMovable()) {
-    TowerFactory::TowerVector.back()->SetCenter(GetCursorPosX(),
-                                                GetCursorPosY());
-  }
-}
-
-void GameManager::OnRButtonDown(UINT nFlags, CPoint point) {}
-
-void GameManager::OnRButtonUp(UINT nFlags, CPoint point) {}
-
-bool isOverlapOtherTower(GameObject t)
-{
-  for (int i=0; i<(int)TowerFactory::TowerVector.size()-1; i++)
-  {
-    if (IsOverlap(*TowerFactory::TowerVector[i], t))
+    void GameManager::OnLButtonUp(UINT nFlags, CPoint point)
     {
-      return true;
     }
-  }
-  return false;
-}
+
+    void GameManager::OnMouseMove(UINT nFlags, CPoint point)
+    {
+        if (!TowerFactory::TowerVector.empty() &&
+            TowerFactory::TowerVector.back()->IsMovable())
+        {
+            TowerFactory::TowerVector.back()->SetCenter(GetCursorPosX(),
+                                                        GetCursorPosY());
+        }
+    }
+
+    void GameManager::OnRButtonDown(UINT nFlags, CPoint point)
+    {
+    }
+
+    void GameManager::OnRButtonUp(UINT nFlags, CPoint point)
+    {
+    }
+
+    bool isOverlapOtherTower(GameObject t)
+    {
+        for (int i = 0; i < static_cast<int>(TowerFactory::TowerVector.size()) - 1; i++)
+        {
+            if (IsOverlap(*TowerFactory::TowerVector[i], t))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 
     void GameManager::OnMove()
     {
@@ -116,9 +125,10 @@ bool isOverlapOtherTower(GameObject t)
         }
         map->UpdateFactoryButton();
 
-  switch (GameFlow) {
-  case Prepare:
-    break;
+        switch (GameFlow)
+        {
+        case Prepare:
+            break;
 
         case Shoot:
             {
@@ -150,23 +160,27 @@ bool isOverlapOtherTower(GameObject t)
                 // todo gold ++
             }
 
-    break;
-  case GameEnd:
-    // todo print win or lose and can restart
-    if (IsLose) {
-      // todo print lose
-    } else {
-      // todo print win
-    }
+            break;
+        case GameEnd:
+            // todo print win or lose and can restart
+            if (IsLose)
+            {
+                // todo print lose
+            }
+            else
+            {
+                // todo print win
+            }
 
-    break;
-  default:;
-  }
-  for (auto &m : TowerFactory::TowerVector) {
-    m->Update();
-  }
-  BloonFactory::UpdateBloon();
-}
+            break;
+        default: ;
+        }
+        for (auto& m : TowerFactory::TowerVector)
+        {
+            m->Update();
+        }
+        BloonFactory::UpdateBloon();
+    }
 
     void GameManager::OnShow()
     {
@@ -191,25 +205,33 @@ bool isOverlapOtherTower(GameObject t)
         }
     }
 
-void GameManager::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags) {
-  if (nChar == 'A') {
-    BloonFactory::MakeBloon(Layer::red);
-  }
-  if (nChar == 'S') {
-    BloonFactory::MakeBloon(Layer::blue);
-  }
-  if (nChar == 'D') {
-    BloonFactory::MakeBloon(Layer::green);
-  }
-  if (nChar == 'F') {
-    BloonFactory::MakeBloon(Layer::yellow);
-  }
-  if (nChar == 'Z') {
-    BloonFactory::MakeBloon(Layer::black);
-  }
-  if (nChar == 'X') {
-    BloonFactory::MakeBloon(Layer::white);
-  }
-}
-         shared_ptr<Map> GameManager::map;
+    void GameManager::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
+    {
+        if (nChar == 'A')
+        {
+            BloonFactory::MakeBloon(Layer::red);
+        }
+        if (nChar == 'S')
+        {
+            BloonFactory::MakeBloon(Layer::blue);
+        }
+        if (nChar == 'D')
+        {
+            BloonFactory::MakeBloon(Layer::green);
+        }
+        if (nChar == 'F')
+        {
+            BloonFactory::MakeBloon(Layer::yellow);
+        }
+        if (nChar == 'Z')
+        {
+            BloonFactory::MakeBloon(Layer::black);
+        }
+        if (nChar == 'X')
+        {
+            BloonFactory::MakeBloon(Layer::white);
+        }
+    }
+
+    shared_ptr<Map> GameManager::map;
 } // namespace Btd
