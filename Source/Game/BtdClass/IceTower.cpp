8597b00fbf8edf3df1414a678ffee4444d3a922f
huhuhu0420 <ghostcheng.00@gmail.com>
2023-04-08 12:39:08 +0800

    ice can set scale

--- Source/Game/BtdClass/IceTower.cpp
+++ Source/Game/BtdClass/IceTower.cpp
@@ -8,6 +8,7 @@ namespace Btd
     IceTower::IceTower()
     {
         _range = 300;
+        _iceScale = 1;
         ThrowablePath = {"resources/towers/ice/ice.bmp"};
     }
 
@@ -19,5 +20,20 @@ namespace Btd
         iceGas->SetMaxExistTime(150);
         iceGas->SetPenetrate(true);
         throwablePool.push(iceGas);
+    }
+
+    void IceTower::TowerShow()
+    {
+        if (_isClicked)
+        {
+            this->RangeCircle.ShowBitmap((float)_range / 100.0);
+        }
+        for (int i=0; i<(int)throwables.size(); i++)
+        {
+            throwables[i]->SetCenter((int)GetCenter().X - static_cast<int>(75 * (_iceScale - 1)),
+                (int)GetCenter().Y - static_cast<int>(75 * (_iceScale - 1)));
+            throwables[i]->ShowBitmap(_iceScale);
+        }
+        this->ShowBitmap();
     }
 }
